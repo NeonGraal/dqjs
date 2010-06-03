a990a11c81c66fb34274a0d9e27d974276f0a028
struan@scifi.geek.nz <struan@scifi.geek.nz@257f0db6-c24b-11dd-b31d-17274aa020d1>
2010-06-03 23:41:06 +0000

    Workup test.html and basic server

--- trunk/server.pl
+++ trunk/server.pl
@@ -3,7 +3,7 @@ use strict;
 $| = 1;
 
 use HTTP::Daemon;
-use HTTP::Status;
+use HTTP::Status qw(:constants);
 
 my($F, $path);
 my $d = HTTP::Daemon->new || die;
@@ -11,23 +11,41 @@ print "Please contact me at: <URL:", $d->url, ">\n";
 system "start ".$d->url;
 while (my $c = $d->accept) {
 	while (my $r = $c->get_request) {
+		$path = substr($r->uri->path, 1);
 		if ($r->method eq 'GET') {
-			$path = substr($r->uri->path, 1);
 			if ($r->uri->path eq "/") {
 				$c->send_file_response("default.htm");
 			} elsif (-r $path) {
 				$c->send_file_response($path);
+			} elsif ($path=~/\*/) {
+				my @files = glob($path);
+				$c->send_response(HTTP_OK, 'OK', undef, '["'.join('","', @files).'"]');
 			} else {
-				print "Unable to find '".$path."'\n";
+				$c->send_error(HTTP_NOT_FOUND);
 			}
-		}
-		elsif ($r->method eq 'PUT' and $r->uri->path =~ /\.dq$/) {
+		} elsif ($r->method eq 'PUT' and $path =~ /\.dq$/) {
 			undef($F);
-			open $F, '>:utf8', $r->uri->path;
-			print $F $r->content;
-			close $F;
+			eval {
+				open $F, '>:utf8', $path;
+				print $F $r->content;
+				close $F;
+			};
+			if ($@) {
+				$c->send_error(HTTP_INTERNAL_SERVER_ERROR, $@);
+			} else {
+				$c->send_file_response($path);
+			}
+		} elsif ($r->method eq 'DELETE' and $path =~ /\.dq$/) {
+			eval {
+				unlink $path;
+			};
+			if ($@) {
+				$c->send_error(HTTP_INTERNAL_SERVER_ERROR, $@);
+			} else {
+				$c->send_response();
+			}
 		} else {
-			$c->send_error(RC_FORBIDDEN)
+			$c->send_error(HTTP_FORBIDDEN);
 		}
 	}
 	$c->close;
