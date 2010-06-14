4e0e4625d18a830f65db09cea3c83af154baf75f
struan@scifi.geek.nz <struan@scifi.geek.nz@257f0db6-c24b-11dd-b31d-17274aa020d1>
2010-06-14 22:27:13 +0000

    Basic Stats display

--- dqChar.js
+++ dqChar.js
@@ -1,10 +1,28 @@
-var $dqChars, $output;
+var $dqChars, $output, $dqChar;
 $(function() {
+	function showChar(dq) {
+		var sH, sV;
+		function showStat(stat) {
+			sH[sH.length] = '<TH>'+stat+'</TH>';
+			sV[sV.length] = '<TD class="stat" id="my'+stat+'">'+dq.Stats[stat]+'</TD>';
+		}
+		return function(sT,tS,xhr) {
+			sH = []; sV = [];
+			showStat('PS');showStat('MD');showStat('AG');
+			showStat('WP');showStat('MA');showStat('EN');
+			showStat('FT');showStat('PC');showStat('PB');
+			$('#statHead').append(sH.join(''));
+			$('#statVal').append(sV.join(''));
+		};
+	}
+
 	$dqChars = $('#dqCharacters');
+	$dqChar = $('#dqChar');
 	$output = $('#output');
-	
+
 	$dqChars.bind('change', function(e) {
 		$.getJSON($(this).val()+'.dq', function(d,s) {
+			$dqChar.load('dqChar.html #dqChar-All', showChar(d));
 			$output.text($.toJSON(d));
 		});
 	});
