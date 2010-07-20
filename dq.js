cce2bf5499be236f828f54927bc611325cd12309
struan@scifi.geek.nz <struan@scifi.geek.nz@257f0db6-c24b-11dd-b31d-17274aa020d1>
2010-07-20 23:20:17 +0000

    Move stats table to Stats.html and improve sub-HTML loading

--- dq.js
+++ dq.js
@@ -5,5 +5,15 @@ var DQ = {
 				for (var i=0; i<base.mods.length; i++)
 					val += val + base.mods[i].val;
 			return val;
+		},
+	load: function load(id,html,cb) {
+		var $e = $(id);
+		if ($e.children().length > 0) cb();
+		else $e.load(html, cb);
+	},
+	script: function script(script) {
+			$.getScript('dq/'+script+'.js',function(d,s){
+					console.log('dq/'+script+'.js loaded: '+s);
+				});
 		}
 };
