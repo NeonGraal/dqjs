06bd575c80b6595126353af06e87142073efcaa5
struan@scifi.geek.nz <struan@scifi.geek.nz@257f0db6-c24b-11dd-b31d-17274aa020d1>
2010-07-20 22:37:13 +0000

    Begin split of functionality out into sub-files

--- trunk/dqChar.js
+++ trunk/dqChar.js
@@ -4,24 +4,10 @@ var Char;
 $(function() {
 	function showChar(char) {
 		var sV;
-		function statCurr(stat) {
-			return DQ.applyMods(char.Stats[stat]);
-		}
-		function statCheck(stat, mul) {
-			return statCurr(stat) * mul;
-		}
-		function showStat(stat) {
-			sV[sV.length] =
-				'<TR><TD class="stat">'+stat+'</TD><TD id="my'+stat+'">'+char.Stats[stat]+'</TD>' +
-				'<TD id="curr'+stat+'">'+statCurr(stat)+'</TD>' + '<TD>'+statCheck(stat,1)+'</TD>' +
-				'<TD>'+statCheck(stat,2)+'</TD>' + '<TD>'+statCheck(stat,3)+'</TD>' +
-				'<TD>'+statCheck(stat,4)+'</TD>' + '<TD>'+statCheck(stat,5)+'</TD></TR>';
-		}
 		return function(sT,tS,xhr) {
 			sV = [];
-			showStat('PS');showStat('MD');showStat('AG');
-			showStat('WP');showStat('MA');showStat('EN');
-			showStat('FT');showStat('PC');showStat('PB');
+			for (var stat in DQ.Stats)
+				sV[sV.length] = DQ.showStat(char,DQ.Stats[stat]);
 			$('#tblStats').append(sV.join(''));
 		};
 	}
@@ -29,6 +15,10 @@ $(function() {
 	$dqChars = $('#dqCharacters');
 	$dqChar = $('#dqChar');
 	$output = $('#output');
+
+	$.getScript('dq/Stats.js',function(d,s){
+			console.log('dq/Stats.js loaded: '+s);
+		});
 
 	$dqChars.bind('change', function(e) {
 		$.getJSON($(this).val()+'.dq', function(char,s) {
